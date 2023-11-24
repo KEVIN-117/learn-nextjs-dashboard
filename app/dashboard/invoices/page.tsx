0adeba59e60b8a20dd4a1b9ce5448ef5195e30ac
Kevin Alexis Rodriguez Condori <sis414.kevinrodriguez17@gmail.com>
2023-11-23 23:59:29 -0400

    chapter 5: navigation between pages

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Page() {
+    // /*#__PURE__*/React.createElement("p", null, "Invoices Page");
+    return (
+        <p>Invoices Page</p>
+    )
+}
